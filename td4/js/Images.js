4617fd60bbad1ac232499e6ab408ede7c86ab051
unknown <anaelle.bargas@sts-sio-caen.info>
2023-11-09 20:39:10 +0100

    images.js finished

--- td4/js/Images.js
+++ td4/js/Images.js
@@ -1,5 +1,6 @@
-const photos = []
-
-function AfficheImage(){
-
+var currentImage = "dolomites-2897227";
+function AfficheImage(newCurrentImage){
+    document.getElementById(currentImage).style.display="none";
+    document.getElementById(newCurrentImage).style.display="flex";
+    currentImage = newCurrentImage;
 }
